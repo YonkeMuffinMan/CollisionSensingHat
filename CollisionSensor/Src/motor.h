65c1065e879f81d9876058958c42e694578c3693
ehrouda <ehrouda@gmail.com>
2020-03-29 11:36:42 -0600

    Motor Speed Vaired
    Closes #7
    Moved PWM setup into motor.c and motor.h
    Added function setDutyCycle which sets the duty cycle to that percentage
    Right now, motor assumes on pin GPIOB, TIM3, PWM CH1, and pin AF1

---
+++ CollisionSensor/Src/motor.h
@@ -0,0 +1,13 @@
+#include "stm32f0xx_hal.h"
+
+typedef struct motor {
+	uint8_t pin_number;
+	uint16_t pwm_prescalar;
+	uint16_t pwm_arr;
+} MOTOR;
+
+void setupMotor(MOTOR motor);
+void startMotor(void);
+void configGPIOB_AF1(uint8_t x);
+
+void setDutyCycle(float prcnt);
